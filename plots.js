650431bdcda893a0c701ec7397d9260e7404dc7c
jozhen <89761792+jozhen@users.noreply.github.com>
2021-11-11 18:55:36 -0800

    Add files via upload

---
+++ plots.js
@@ -0,0 +1,21 @@
+//Plotly.newPlot("plotArea", [{x: [1, 2, 3], y: [10, 20, 30]}]);
+
+var sortedCities = cityGrowths.sort((a,b) => a.Increase_from_2016 - b.Increase_from_2016).reverse(); 
+
+var topFiveCityNames = cityGrowths.map(city => city.City);
+var topFiveCityGrowths = cityGrowths.map(city => parseInt(city.Increase_from_2016));
+
+
+var trace = {
+x: topFiveCityNames,
+y: topFiveCityGrowths,
+type: "bar"
+};
+var data = [trace];
+var layout = {
+    title: "Most Rapidly Growing Cities",
+    xaxis: { title: "City" },
+    yaxis: { title: "Population Growth, 2016-2017"}
+};
+  
+Plotly.newPlot("bar-plot", data, layout);
